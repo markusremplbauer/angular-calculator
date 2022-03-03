5da61f3691f5d527001104fca0673dcd22837d12
markusremplbauer <markus.remplbauer@gmx.at>
2022-03-03 11:58:24 +0100

    implement calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calculator';
+  title = 'calculator'
+  input: string = ''
+  numb: number = 0
+  remove: boolean = false;
+
+
+  updateValue(number: string) {
+    if(this.remove){
+      this.input = ''
+      this.remove = false;
+    }
+    this.input += number
+  }
+
+  handlePlus() {
+    this.numb = +this.input
+    this.remove = true
+  }
+
+  handleEvaluate() {
+    this.input = (+this.input + this.numb).toString()
+    this.numb = 0
+  }
+
+  clearInput() {
+    this.input = ''
+  }
 }
